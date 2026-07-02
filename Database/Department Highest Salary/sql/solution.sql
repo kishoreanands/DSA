e599fd3676ccc4bb5087d5bc6133f86ccb403a86
Kishore Anand S <kishoreanand876@gmail.com>
2026-07-02 10:56:43 +0530

    feat: Add Department Highest Salary

---
+++ Database/Department Highest Salary/sql/solution.sql
@@ -0,0 +1,5 @@
+# Write your MySQL query statement below
+SELECT d.name as Department,e.name as Employee,e.salary as Salary FROM Employee e
+left join Department d 
+on d.id=e.departmentId
+where e.salary=(select max(salary) from Employee e where e.departmentId=d.id);
