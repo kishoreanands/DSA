c23d19cdde8dd68e97ef5afaa776bfe24494ab42
Kishore Anand S <kishoreanand876@gmail.com>
2026-07-02 09:51:24 +0530

    feat: Add Department Highest Salary

--- Database/Department Highest Salary/sql/solution.sql
+++ Database/Department Highest Salary/sql/solution.sql
@@ -1,4 +1,5 @@
 # Write your MySQL query statement below
-SELECT departmentId, MAX(salary)
-FROM Employee
-GROUP BY departmentId;
+SELECT DEPT.name AS Department, EMP.name AS Employee, EMP.salary AS 
+Salary FROM Department DEPT, Employee EMP WHERE
+EMP.departmentId = DEPT.id AND (EMP.departmentId, salary) IN 
+(SELECT departmentId, MAX (salary) FROM Employee GROUP BY departmentId)
