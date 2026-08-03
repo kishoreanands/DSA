740733fa12e6621a6fead9ca010a8201679490d4
Kishore Anand S <kishoreanand876@gmail.com>
2026-08-03 11:20:39 +0530

    feat: Add Valid Perfect Square

--- Math/Valid Perfect Square/java/Solution.java
+++ Math/Valid Perfect Square/java/Solution.java
@@ -1,11 +1,10 @@
 class Solution {
     public boolean isPerfectSquare(int num) {
         boolean bool=false;
-        for(int i=1;i<num;i++){
-           if (i * i == num) {
-                return true;
-            }
-        }
-        return bool;
+        for (long i = 1; i * i <= num; i++) {
+             if (i * i == num)
+                 return true;
+          }
+        return false;
     }
 }
