8ccb9439ef839e56868ec0d044c296819562b823
Kishore Anand S <kishoreanand876@gmail.com>
2026-08-03 10:29:49 +0530

    feat: Add Number of Segments in a String

--- String/Number of Segments in a String/java/Solution.java
+++ String/Number of Segments in a String/java/Solution.java
@@ -2,11 +2,15 @@ class Solution {
     public int countSegments(String s) {
        char arr[]=s.toCharArray();
        int count=0;
+       int t=0;
        for(int i=0;i<arr.length;i++){
         if(arr[i]==' '){
+            t=0;
+        }if(arr[i]!=' '&&t==0){
             count++;
+            t=1;;
         }
        }
-       return count+1;
+       return count;
     }
 }
