02de14251d397f2032f2992d65a8b5852e08afe5
Kishore Anand S <kishoreanand876@gmail.com>
2026-08-03 11:44:24 +0530

    feat: Add Convert a Number to Hexadecimal

--- Math/Convert a Number to Hexadecimal/java/Solution.java
+++ Math/Convert a Number to Hexadecimal/java/Solution.java
@@ -1,5 +1,21 @@
 class Solution {
-    public String toHex(int num) {
-      return Integer.toHexString(num);  
+char[] hex = {
+    '0','1','2','3','4','5','6','7',
+    '8','9','a','b','c','d','e','f'
+};    public String toHex(int num) {
+        if(num==0){
+            return "0";
+        }
+        long n=num;
+        if(n<0){
+            n+=(1l<<32);
+        }
+        return convert(n);
+    }
+    private String convert(long n){
+        if(n==0){
+            return "";
+        }
+        return convert(n/16)+hex[(int)(n%16)];
     }
 }
