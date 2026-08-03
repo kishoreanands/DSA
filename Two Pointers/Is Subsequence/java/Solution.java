22bd0b104e39c8d45428f6f2d9ba680a53e64d3c
Kishore Anand S <kishoreanand876@gmail.com>
2026-08-03 11:31:01 +0530

    feat: Add Is Subsequence

--- Two Pointers/Is Subsequence/java/Solution.java
+++ Two Pointers/Is Subsequence/java/Solution.java
@@ -1,20 +1,17 @@
 class Solution {
     public boolean isSubsequence(String s, String t) {
-
-        if (s.length() == 0)
-            return true;
-
-        int k = 0;
-
-        for (int i = 0; i < t.length(); i++) {
-            if (s.charAt(k) == t.charAt(i)) {
-                k++;
-                if (k == s.length()) {
-                    break;
-                }
+      int k=0;
+       if(s.length()==0){
+        return false;
+       }
+      for(int i=0;i<t.length();i++){
+        if(s.charAt(k)==t.charAt(i)){
+            k++;
+            if(k>=s.length()){
+                break;
             }
         }
-
-        return k == s.length();
+      }
+      return k==s.length();
     }
 }
